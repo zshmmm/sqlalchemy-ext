788656a8f1d0919003d85cf7efc5f9edbbb0caea
zshmmm <30523755+zshmmm@users.noreply.github.com>
2018-10-26 14:46:27 +0800

    Update sqlalchemy-ext-query.py

--- sqlalchemy-ext-query.py
+++ sqlalchemy-ext-query.py
@@ -1,11 +1,7 @@
 # coding: utf-8
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-Base = declarative_base()
-metadata = Base.metadata
 
 def conntodb():
     dbconnurl = ('mysql+pymysql://{user}:{passwd}@{host}:{port}'
